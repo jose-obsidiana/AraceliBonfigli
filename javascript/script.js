a0f7a097d0944d718f05d6053b8412fee8bd6deb
María José Oliveira <proyectos.joseoliveira@gmail.com>
2024-01-20 19:47:50 -0300

    reproductor funcionando

--- javascript/script.js
+++ javascript/script.js
@@ -1,81 +1,29 @@
+// dom
+const inputNombre = document.getElementById('name_input')
+const inputApellido = document.getElementById('surname_input')
+const inputTelefono = document.getElementById('telephone_input')
+const inputEmail = document.getElementById('email_input')
+const inputMensaje = document.getElementById('message_input')
+const botonEnviar = document.getElementById('form_button')
+
+botonEnviar.addEventListener('click', function(event) {
+    event.preventDefault()
+    guardarDatos()
+})
 
 
-// let container = document.getElementById("container");
+// guardar datos en localstorage
+function guardarDatos() {
+    const datosContacto = {
+        nombre: inputNombre.value,
+        apellido: inputApellido.value,
+        telefono: inputTelefono.value,
+        email: inputEmail.value,
+        mensaje: inputMensaje.value
+    }
 
-// //console.log(container.textContent)
-
-// let mensajePersonas = ['homero', 'marge', 'bart', 'lisa', 'magie']
-
-// for (const x of mensajePersonas) {
-//     let lista = document.createElement("p")
-//     lista.innerHTML = x
-//     container.appendChild(lista)
-// }
-
-
-
-/*   SINTAXIS VIEJA (opcion 2)
-
-
-let button = document.getElementById("form_button")
-
-function mensajeEnviado() {
-    alert('Tu mensaje ha sido enviado!')
+    let resultadoDatos = JSON.stringify(datosContacto)
+    localStorage.setItem('datosContacto', resultadoDatos)
+    console.log('resultadoDatos')
 }
-
-button.onclick = () => {mensajeEnviado()} */
-
-
-
-// SINTAXIS NUEVA (opcion 1)
-
-// let button = document.getElementById("form_button")
-
-// function mensajeEnviado() {
-//     alert('Tu mensaje ha sido enviado!')
-// }
-
-// button.addEventListener('click', mensajeEnviado)
-
-
-
-
-
-// GENERAR LA FUNCION DENTRO DEL EVENTO QUE QUIERO DISPARAR
-
-// let boton = document.getElementById("form_button")
-
-// boton.addEventListener( 'submit', () => {alert('Tu mensaje ha sido enviado!')} )
-
-
-
-// MOSTRAR EN CONSOLA EN TIEMPO REAL LO QUE ESCRIBO
-// let formulario = document.getElementById("contact_form")
-
-// formulario.addEventListener("input", function(event) {
-//     console.log(event.target.value)
-
-// })
-
-
-
-// ENVIAR FORMULARIO
-
-// let formulario = document.getElementById("contact_form")
-
-// formulario.addEventListener("submit", function(event) {
-
-// event.preventDefault() // detiene el envio
-
-//     if (event.target.value === null) {
-//         alert('Te faltaron datos')
-//     }
-
-//     else {
-//         alert('Tu mensaje ha sido enviado!')
-//     }
-// })
-
-
-
 
